2bf3b46be0bd8c30e88bb87490505ec1240412d6
Rohit <ginnorerohit@gmail.com>
2025-10-07 17:46:28 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,21 @@
+const button = document.querySelectorAll('.button');
+// console.log(button);
+const body = document.querySelector("body");
+button.forEach(function(button){
+    console.log(button); 
+    button.addEventListener('click', function(e){
+                 console.log(e);
+                 console.log(e.target);
+           if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id;
+           } else if(e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id;
+           }     
+            else if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+           }     
+            else {
+            body.style.backgroundColor = 'yellow';
+           }     
+    })   
+}) 
